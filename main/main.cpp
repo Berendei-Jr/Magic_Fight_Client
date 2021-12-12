3c4670b8dc8037c75c659bab6c7e06e64abc8c07
Andrey <vedvedved2003@gmail.com>
2021-12-12 23:42:53 +0300

    beta v0.4 (client created)

--- main/main.cpp
+++ main/main.cpp
@@ -1,17 +1,59 @@
 #include <NetClient.hpp>
+#include <SFML/Graphics.hpp>
 
 int main() {
     net::client_interface Client;
-    Client.Connect("127.0.0.1", 60000);
-    while (true)
-    {
-        if (Client.Ready())
-        {
-          //  std::cout << "size: " << Client.NumOfMsgAvailable() << std::endl;
-            std::cout << "MSG GOT: " << Client.Get() << std::endl;
-        }
-        Client.Send("Hello bitch");
-        std::this_thread::sleep_for(std::chrono::seconds(10));
-    }
+    Client.Connect("127.0.0.1", 6969);
 
+    char d, act_key;
+    bool b = false;
+
+    std::map<char, int> direct = {{'a', 0},
+                                  {'w', 1},
+                                  {'e', 2},
+                                  {'d', 3},
+                                  {'x', 4},
+                                  {'z', 5},
+                                  {'s', 6}};
+    std::map<char, int> action_map = {{'k', 0},
+                                      {'o', 1},
+                                      {'l', 2},
+                                      {'p', 3}};
+    std::map<char, int>::iterator it1, it2;
+    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+
+    //! БЫДЛОКОД
+    while (window.isOpen()) {
+
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+            if (event.type == sf::Event::KeyPressed) {
+                d = event.key.code + 97;
+                if (b) {
+                    it1 = direct.find(d);
+                    if (it1 != direct.end())
+                    {
+                        Client.Send(std::to_string(it2->second)
+                        + " " + std::to_string(it1->second));
+                        std::cout << it2->second <<
+                                  " " << it1->second << "\n";
+                    }
+                    b = false;
+                } else {
+                    act_key = d;
+                    it2 = action_map.find(act_key);
+                    b = it2 != action_map.end();
+                }
+
+            }
+
+            window.clear(sf::Color::Black);
+
+
+            window.display();
+        }
+    }
 }
